6db1e25d5e04e6c8a1dfb9ff1b82ac81e21fd858
Sergey <kuzmenkos1986@gmail.com>
2015-10-26 16:49:40 +0200

    news reviews

--- js/jquery.main.js
+++ js/jquery.main.js
@@ -31,6 +31,13 @@ Shablon.prototype = {
 
 $(window).on({
     load: function () {
-        
+        var myMap;
+        ymaps.ready(init); // Ожидание загрузки API с сервера Яндекса
+        function init () {
+            myMap = new ymaps.Map("map", {
+                center: [55.76, 37.64], // Координаты центра карты
+                zoom: 10 // Zoom
+            });
+        }
     }
 });
